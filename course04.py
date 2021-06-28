61fd0fc81b21ccce9564cffbac9f0e4d7e2273d0
DragosSHD <dragos.dragos.502@gmail.com>
2021-06-28 19:05:54 +0300

    Fourth Course

--- course04.py
+++ course04.py
@@ -6,20 +6,4 @@
 my_sum = (lambda param1, param2: param1 + param2)(5, 7)
 print('my_sum', my_sum)
 
-# use case example:
-
-students = [{
-    'name': 'Student 1',
-    'grade': 7.85,
-}, {
-    'name': 'Student 2',
-    'grade': 6.75,
-}, {
-    'name': 'Student 3',
-    'grade': 10,
-}, {
-    'name': 'Student 4',
-    'grade': 8.44,
-}]
-
-students.sort(key=kambda student_item: student_item['grade'] if
+# - map: 
