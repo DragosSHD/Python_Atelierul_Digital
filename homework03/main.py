a427789272c653e6b684d23cc77d6ec8e49e0639
DragosSHD <dragos.dragos.502@gmail.com>
2021-07-01 13:55:58 +0300

    Homework 3

--- homework03/main.py
+++ homework03/main.py
@@ -1,5 +1,6 @@
 from web_scrapper import *
 
 print('Hi! Let\'s start web scrapping. Enter the category link below: ')
+print('Example link: https://xtrems.ro/84-casti')
 crt_url = input()
 get_product_list(crt_url)
